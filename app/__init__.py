0f7f9a2b253617297293c80a4153d56142d8333f
Nicolas Ruiz <syngenruiz2@gmail.com>
2023-06-28 22:18:56 -0600

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="John Doe", url=os.getenv("URL"))
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About", url=os.getenv("URL"))
+
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', title="Hobbies", url=os.getenv("URL"))
+
+
+@app.route('/education')
+def education():
+    return render_template('education.html', title="Education", url=os.getenv("URL"))
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title="Projects", url=os.getenv("URL"))
