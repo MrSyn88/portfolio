4572879ff0d1405682a77894301c24e18cc8971d
Nicolas Ruiz <syngenruiz2@gmail.com>
2023-07-14 05:11:31 -0500

    commented out new lines to test their necessity

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,16 +3,16 @@
 tmux kill-server
 cd portfolio
 git fetch && git reset origin/main --hard
-read -p "MySQL Host: " MYSQL_HOST
-read -p "MySQL User: " MYSQL_USER
-read -s -p "MySQL Password: " MYSQL_PASSWORD
-read -p "MySQL Database: " MYSQL_DATABASE
-echo "Writing .env file..."
-echo "URL=localhost:5000" > .env
-echo "MYSQL_HOST=$MYSQL_HOST" >> .env
-echo "MYSQL_USER=$MYSQL_USER" >> .env
-echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >> .env
-echo "MYSQL_DATABASE=$MYSQL_DATABASE" >> .env
+# read -p "MySQL Host: " MYSQL_HOST
+# read -p "MySQL User: " MYSQL_USER
+# read -s -p "MySQL Password: " MYSQL_PASSWORD
+# read -p "MySQL Database: " MYSQL_DATABASE
+# echo "Writing .env file..."
+# echo "URL=localhost:5000" > .env
+# echo "MYSQL_HOST=$MYSQL_HOST" >> .env
+# echo "MYSQL_USER=$MYSQL_USER" >> .env
+# echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >> .env
+# echo "MYSQL_DATABASE=$MYSQL_DATABASE" >> .env
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 tmux new -d -s prod 'cd ~/portfolio;source python3-virtualenv/bin/activate;flask run --host=0.0.0.0'
