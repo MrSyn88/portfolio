7fa1f4876f0ebfa7047c8985b87f6cee1e46e69d
Nicolas Ruiz <syngenruiz2@gmail.com>
2023-07-16 00:34:08 -0500

    remove comments from redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,16 +3,6 @@
 
 cd portfolio
 git fetch && git reset origin/main --hard
-# read -p "MySQL Host: " MYSQL_HOST
-# read -p "MySQL User: " MYSQL_USER
-# read -s -p "MySQL Password: " MYSQL_PASSWORD
-# read -p "MySQL Database: " MYSQL_DATABASE
-# echo "Writing .env file..."
-# echo "URL=localhost:5000" > .env
-# echo "MYSQL_HOST=$MYSQL_HOST" >> .env
-# echo "MYSQL_USER=$MYSQL_USER" >> .env
-# echo "MYSQL_PASSWORD=$MYSQL_PASSWORD" >> .env
-# echo "MYSQL_DATABASE=$MYSQL_DATABASE" >> .env
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 systemctl daemon-reload
