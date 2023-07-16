9e4a76abf19dbb97f06e19d19427b6db3b4822aa
Nicolas Ruiz <syngenruiz2@gmail.com>
2023-07-16 00:30:10 -0500

    fix for redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -16,4 +16,4 @@ git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 systemctl daemon-reload
-system restart myportfolio
+systemctl restart myportfolio
