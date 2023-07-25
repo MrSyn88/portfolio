cfbeff1a12e20092f5edce2972e3fc529784df8a
Nicolas Ruiz <syngenruiz2@gmail.com>
2023-07-25 18:33:25 -0500

    small tweaks

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo "Deploying..."
-if 
-! cd portfolio || exit
+printf "Deploying...\n\n"
+if
+! cd portfolio
 ! git fetch && git reset origin/main --hard
 ! source python3-virtualenv/bin/activate
 ! pip install -r requirements.txt
 ! systemctl daemon-reload
 ! systemctl restart myportfolio
 then
-     echo "Deployment FAILED"
+     printf "\n\nDeployment FAILED\n"
 else
-     echo "Deployment Success"
+     printf "\n\nDeployment SUCCESS\n"
 fi
