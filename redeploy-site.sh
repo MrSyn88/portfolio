cf98bd2e7107268bb13613c84692f0bfbfe544a6
Nicolas Ruiz <syngenruiz2@gmail.com>
2023-07-25 18:21:30 -0500

    deployment edit

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,15 @@
-#!/bin/zsh
+#!/bin/bash
 
-
-cd portfolio
-git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
+echo "Deploying..."
+if 
+! cd portfolio || exit
+! git fetch && git reset origin/main --hard
+! source python3-virtualenv/bin/activate
+! pip install -r requirements.txt
+! systemctl daemon-reload
+! systemctl restart myportfolio
+then
+     echo "Deployment FAILED"
+else
+     echo "Deployment Success"
+fi
