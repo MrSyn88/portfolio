7915ea8136bd655a43c534cb71b32c65bb236201
Nicolas Ruiz <syngenruiz2@gmail.com>
2023-07-27 11:19:34 -0500

    fix redeploy script and cleanup

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -5,7 +5,7 @@ if
 ! cd portfolio
 ! git fetch && git reset origin/main --hard
 ! docker compose -f docker-compose.prod.yml down
-! docker compose -f docker_compose.prod.yml up -d --build
+! docker compose -f docker-compose.prod.yml up -d --build
 then
      printf "\n\nDeployment FAILED\n"
 else
